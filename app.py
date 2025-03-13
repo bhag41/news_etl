b0e1cf045911dcd8a7e160c3f7d8f2d55903acd3
Bhagyashree Kulkarni <35288783+bhag41@users.noreply.github.com>
2025-03-13 11:00:16 -0700

    extracted news about doge

--- app.py
+++ app.py
@@ -6,3 +6,19 @@ from newsapi import NewsApiClient
 news_api_key = "10e55a1639ed4d509b1f3f0dbf1b62e4"
 newsapi = NewsApiClient(api_key=news_api_key)
 
+def extract_news_date():
+    try:
+        top_headlines = newsapi.get_top_headlines(q='doge', language='en')
+        articles = top_headlines['articles']
+        logging.info("Extracted news")
+        news = []
+        for article in articles:
+            news.append([article['title'], article['description'], article['url']])
+        return news
+    except:
+        logging.error("Error in extracting news")
+        return None
+    
+articles = extract_news_date()
+
+print(articles[:5])
